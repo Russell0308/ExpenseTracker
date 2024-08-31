a266929e4e1577c4051b40d9e1bbf9d9865a3487
Russell Salazar <83458251+Russell0308@users.noreply.github.com>
2024-08-31 19:41:36 -0400

    paymethod check added

--- log_purchase.py
+++ log_purchase.py
@@ -1,11 +1,8 @@
 ### import datetime
 
 
-def input_check(date, time, description, amount, payment_method, merchant_name, location, recurring):
-    check_list = []
 
-
-    ### Date check
+def check_date(date):
     date_check = True
 
     if date == 'today' or date == 'Today':
@@ -18,7 +15,6 @@ def input_check(date, time, description, amount, payment_method, merchant_name, 
             pass
         else:
             date_check = False
-            check_list.append(date_check)
     elif len(date) == 4:
         month = date[:2]
         day = date[2:]
@@ -26,15 +22,13 @@ def input_check(date, time, description, amount, payment_method, merchant_name, 
             pass
         else:
             date_check = False
-            check_list.append(date_check)
     else:
         date_check = False
-        check_list.append(date_check)
         
-    check_list.append(date_check)
+    return date_check
 
 
-    ### time_check   FORMAT | 01:01p 
+def check_time(time):
     time_check = True
     
     if len(time) != 6:       ### Remove to make input easier
@@ -46,32 +40,55 @@ def input_check(date, time, description, amount, payment_method, merchant_name, 
     elif not time[5] == 'p' and not time[5] == 'a':   
         time_check = False
 
+    return time_check
 
-    check_list.append(time_check)
 
-
-    ### description
+def check_description(description):
     description_check = True
 
     description = str(description)
     
-    check_list.append(description_check)
+    return description_check
 
-    ### amount_check
+
+def check_amount(amount):
     amount_check = True
 
     try: 
         int(amount)
-        check_list.append(amount_check)
+        return amount_check
     except Exception:
         print(Exception)
 
-    return check_list
 
-    ### payment_method
-    
+def check_paymethod(payment_method, names):
+    paymethod_check = True
+
+    for i in names:
+        if i == payment_method:
+            return paymethod_check
+        else:
+            paymethod_check = False
+    return paymethod_check
+
+
+def check_input(date, time, description, amount, payment_method, paymethod_names, merchant_name, location, recurring):
+    check_list = []
+
+    checked_date = check_date(date)
+    check_list.append(checked_date)
+
+    checked_time = check_time(time)
+    check_list.append(checked_time)
+
+    checked_description = check_description(description)
+    check_list.append(checked_description)
+
+    checked_amount = check_amount(amount)
+    check_list.append(checked_amount)
+
+    checked_paymethod = check_paymethod(payment_method, paymethod_names)
+    check_list.append(checked_paymethod)
 
 print(input_check("02/01", "06:34a", "1", "101", "1", "!", "1", "True"))
 
-def main(date, time, description, amount, payment_method, merchant_name, location, recurring):
-    pass
